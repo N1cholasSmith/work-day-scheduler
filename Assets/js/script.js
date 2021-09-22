79df40745f09f1a49c75ef036cbf1237d8edba44
nick smith <n1ckorish14@gmail.com>
2021-09-22 20:03:06 +1000

    updated js and html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,49 @@
 
-var myModal = new bootstrap.Modal(document.getElementById('task-modal'))
+// current time -----------------------------------------------------------------------
+var time = moment();
+$("#time-display").text(time.format('dddd,mmm Do YYYY'));
 
+
+// currentTime();
+// setInterval(function(){
+//     currentTime();
+// },1000);
+
+// time tracker -----------------------------------------------------------------------
+
+function timeTracker() {
+
+    var currentHour = moment().hour();
+
+    $(".timeBlock").each(function() {
+
+            var checkHour = $(this).parent().attr("id");
+
+            checkHour = parseInt (checkHour); 
+            if (checkHour < currentHour) {
+                // if > currentHour, text area is grey
+                $(this).siblings().addClass ("past");
+                // removeClass ensures there are no conflicts
+                $(this).siblings().removeClass("present");
+                $(this).siblings().removeClass("future");
+            } else if (checkHour === currentHour) {
+                // 
+                $(this).siblings().addClass ("past");
+                $(this).siblings().removeClass("present");
+                $(this).siblings().removeClass("future");
+            } else {
+                $(this).siblings().addClass ("past");
+                $(this).siblings().removeClass("present");
+                $(this).siblings().removeClass("future");
+        };      
+        
+    })
+
+};
+
+
+// modul ---------------------------------------------------------------------------------
+var myModal = new bootstrap.Modal(document.getElementById('task-modal'))
 var myInput = document.getElementById('task-input')
 
 $("li").on('click', function(event){
@@ -13,7 +56,29 @@ $("#close").on("click", function(event){
     myModal.hide()
 });
 
+// Saving the Task ----------------------------------------------------------------------------------------------
 $("#save").on("click", function(event){
 
 })
 addEventListener.savechanges.on('hide')
+
+function savetask() {
+    var timeSelected = $(this).parent().attr("id");
+    var value = $(this).siblings(".textValue").val();
+    localStorage.setItem(timeSelected, ($(this).siblings(".textValue").val()))
+};
+
+// Retrieving the TextValue from localStorage --------------------------------------------------------------
+
+$("#7 .textValue").val(localStorage.getItem("7"));
+$("#8 .textValue").val(localStorage.getItem("8"));
+$("#9 .textValue").val(localStorage.getItem("9"));
+$("#10 .textValue").val(localStorage.getItem("10"));
+$("#11 .textValue").val(localStorage.getItem("11"));
+$("#12 .textValue").val(localStorage.getItem("12"));
+$("#13 .textValue").val(localStorage.getItem("13"));
+$("#14 .textValue").val(localStorage.getItem("14"));
+$("#15 .textValue").val(localStorage.getItem("15"));
+$("#16 .textValue").val(localStorage.getItem("16"));
+$("#17 .textValue").val(localStorage.getItem("17"));
+$("#18 .textValue").val(localStorage.getItem("17"));
